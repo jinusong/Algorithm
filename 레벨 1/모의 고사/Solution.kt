f26c5527c0e3705de5f579711a1940f4fddc9e28
jinusong <sugwer7464@gmail.com>
2019-12-26 16:20:01 +0900

    [Update] 모의 고사

--- 레벨 1/모의 고사/Solution.kt
+++ 레벨 1/모의 고사/Solution.kt
@@ -1,27 +1,52 @@
+import java.util.*
+import kotlin.collections.ArrayList
+
+
 class Solution {
     fun solution(answers: IntArray): IntArray {
-        var supo1 = intArrayOf(1,2,3,4,5)
-        var supo2 = intArrayOf(2,1,2,3,2,4,2,5)
-        var supo3 = intArrayOf(3,3,1,1,2,2,4,4,5,5)
+        var answer = ArrayList<Int>()
+        val firstStudentAnswers = arrayListOf(1, 2, 3, 4, 5)
+        val secondStudentAnswers = arrayListOf(2, 1, 2, 3, 2, 4, 2, 5)
+        val thirdStudentAnswers = arrayListOf(3, 3, 1, 1, 2, 2, 4, 4, 5, 5)
 
-        var x = 0
-        var y = 0
-        var z = 0
+        var firstStudentCount = 0
+        var secondStudentCount = 0
+        var thirdStudentCount = 0
 
-        var map = mutableMapOf<Int,Int>(Pair(1,0),Pair(2,0),Pair(3,0))
-        for(i in answers) {
-            if(i == supo1[x]) map.put(1,map[1]!! + 1)
-            if(i == supo2[y]) map.put(2,map[2]!! + 1)
-            if(i == supo3[z]) map.put(3,map[3]!! + 1)
-            x = if(x < 4) x + 1 else 0
-            y = if(y < 7) y + 1 else 0
-            z = if(z < 9) z + 1 else 0
+
+        for (i in 0 until answers.size) {
+            firstStudentCount =
+                    if (i >= 5) checkAnswer(firstStudentCount, answers[i], firstStudentAnswers[i%5])
+                    else checkAnswer(firstStudentCount, answers[i], firstStudentAnswers[i])
+
+            secondStudentCount =
+                    if (i >= 8) checkAnswer(secondStudentCount, answers[i], secondStudentAnswers[i%8])
+                    else checkAnswer(secondStudentCount, answers[i], secondStudentAnswers[i])
+
+            thirdStudentCount =
+                    if (i >= 10) checkAnswer(thirdStudentCount, answers[i], thirdStudentAnswers[i%10])
+                    else checkAnswer(thirdStudentCount, answers[i], thirdStudentAnswers[i])
         }
 
-        var max = map.maxBy({it.value})?.value
-        for(i in 1..map.size) if(map[i] != max) map.remove(i)
-        var answer = map.toList().sortedWith(compareBy({it.second})).toMap().keys.toIntArray()
+        val max = Math.max(firstStudentCount, Math.max(secondStudentCount, thirdStudentCount))
 
-        return answer
+        if (max == firstStudentCount) answer.add(1)
+        if (max == secondStudentCount) answer.add(2)
+        if (max == thirdStudentCount) answer.add(3)
+
+        return answer.toIntArray()
+    }
+
+    private fun checkAnswer(nowCount: Int, answer: Int, studentAnswer: Int): Int {
+        if (answer == studentAnswer)
+            return nowCount + 1
+
+        return nowCount
+    }
+}
+
+fun main(args: Array<String>) {
+    Solution().solution(intArrayOf(1,2,3,4,5)).forEach {
+        print(it)
     }
 }
